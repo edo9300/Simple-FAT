57b59623115661be20e694570576932491eb4c84
Edoardo Lolletti <edoardo762@gmail.com>
2022-05-18 18:36:03 +0200

    Add first functions to test program

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include "FAT.h"
+#include <stdio.h>
+#include <assert.h>
 
 int main(int argc, char** argv) {
-	(void)argc;
-	(void)argv;
+	int err;
+	if(argc < 2) {
+		puts("the filename paramter for the disk is required");
+		return 1;
+	}
+	err = initFAT(argv[1]);
+	if(err < 0) {
+		puts("failed to initialize FAT");
+		return 1;
+	}
+
+	err = terminateFAT();
+	assert((err == 0) && "failed to free the resources");
 	return 0;
 }
