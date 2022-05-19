62124f7e657e36b549c4bcafd8bde0c83448a772
Edoardo Lolletti <edoardo762@gmail.com>
2022-05-19 16:20:01 +0200

    Test file creation function

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 
 int main(int argc, char** argv) {
 	int err;
+	int return_code = 0;
+	FileHandle handle;
 	if(argc < 2) {
 		puts("the filename paramter for the disk is required");
 		return 1;
@@ -14,8 +16,15 @@ int main(int argc, char** argv) {
 		perror("failed to initialize FAT");
 		return 1;
 	}
-
+	
+	if(createFileFAT("aaa", &handle) == NULL) {
+		return_code = 1;
+		puts("failed to create file");
+		goto cleanup;
+	}
+	
+cleanup:
 	err = terminateFAT();
 	assert((err == 0) && "failed to free the resources");
-	return 0;
+	return return_code;
 }
