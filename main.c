aa2957783b4a6896832c83aa391a89ff27f69117
Edoardo Lolletti <edoardo762@gmail.com>
2022-05-26 11:33:01 +0200

    Add file read test

--- main.c
+++ main.c
@@ -7,8 +7,10 @@ int main(int argc, char** argv) {
 	int err;
 	int return_code = 0;
 	char a[] = "Test string to write to the file";
+	char read_string[512] = { 0 };
 	FileHandle handle;
 	int written;
+	int read;
 	if(argc < 2) {
 		puts("the filename paramter for the disk is required");
 		return 1;
@@ -26,6 +28,17 @@ int main(int argc, char** argv) {
 	}
 	written = writeFAT(&handle, a, sizeof(a));
 	printf("total written: %d, to write were: %ld\n", written, sizeof(a));
+
+	/*NOTE: use this until seek is implemented*/
+	if(createFileFAT("aaa", &handle) == NULL) {
+		return_code = 1;
+		puts("failed to reopen file");
+		goto cleanup;
+	}
+
+	read = readFAT(&handle, read_string, sizeof(read_string));
+	read_string[read] = 0;
+	printf("total read: %d, to read were: %ld, read content: \"%s\"\n", read, sizeof(read_string), read_string);
 	
 cleanup:
 	err = terminateFAT();
